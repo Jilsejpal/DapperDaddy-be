229998743bf4bc4767a32acdfe8936c6e673309a
Jilsejpal <jilsejpal7@gmail.com>
2023-09-08 11:23:30 +0530

    changes

--- app.js
+++ app.js
@@ -4,16 +4,29 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
+const cors = require("cors");
 const dotenv = require("dotenv");
 //
 dotenv.config({ path: "config/config.env" });
 
 const errorMiddleware = require("./middleware/error");
 
+var whitelist = ["http://localhost", "http://13.235.99.185:3000"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); // support encoded bodies (for forms with
 app.use(fileUpload());
+app.use(cors());
 
 // Route Imports
 const product = require("./routes/productRoute");
